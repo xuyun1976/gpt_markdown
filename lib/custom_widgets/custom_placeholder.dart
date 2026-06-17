05e70581a53ccd6e82c0be1abe198ee6f96b9b84
Xu Yun <>
2026-06-17 14:19:32 +0800

    change the regexp of placeholder

--- lib/custom_widgets/custom_placeholder.dart
+++ lib/custom_widgets/custom_placeholder.dart
@@ -5,7 +5,10 @@ import 'markdown_config.dart';
 
 class PlaceholderMd extends BlockMd {
   @override
-  String get expString => r"\[([A-Za-z_]+):id=([^\]]+)\]";
+  String get expString =>
+      r'!\[[^\]]*\]\((?<type1>[A-Za-z_]+):id=(?<id1>[^)]+)\)'
+      r'|\((?<type2>[A-Za-z_]+):id=(?<id2>[^)]+)\)'
+      r'|\[(?<type3>[A-Za-z_]+):id=(?<id3>[^\]]+)\]';
 
   @override
   Widget build(
@@ -15,17 +18,13 @@ class PlaceholderMd extends BlockMd {
       ) {
     var match = exp.firstMatch(text.trim());
 
-    if (match == null) {
+    if (match == null || config.placeholderBuilder == null) {
       return const SizedBox.shrink();
     }
 
-    String type = match[1]!;
-    String id = match[2]!;
+    final type = match.namedGroup('type1') ?? match.namedGroup('type2') ?? match.namedGroup('type3');
+    final id = match.namedGroup('id1') ?? match.namedGroup('id2') ?? match.namedGroup('id3');
 
-    if (config.placeholderBuilder == null) {
-      return const SizedBox.shrink();
-    }
-
-    return config.placeholderBuilder!(context, type, id, config);
+    return config.placeholderBuilder!(context, type!, id!, config);
   }
 }
